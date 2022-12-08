dc9e0a8908d4236d3c29565500d3ee7a96d3c094
muqeetmughal <muqeetmughal786@gmail.com>
2022-12-08 17:34:10 -0500

    files changed

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 
-gunicorn config.wsgi:application -b :8000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
+gunicorn config.wsgi:application -b :8001 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
